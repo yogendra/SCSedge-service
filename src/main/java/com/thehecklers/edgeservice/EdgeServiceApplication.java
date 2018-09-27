54bfd5743a2a1a56d08a62d13b32d4b686547844
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2018-09-27 08:13:30 +0800

    Update spring cloud and spring cloud service versions

--- src/main/java/com/thehecklers/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/thehecklers/edgeservice/EdgeServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
